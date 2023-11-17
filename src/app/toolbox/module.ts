8906018bf833525b9a9ab9451c8a8ba12dd9bfc7
YangYunfei <yyfcpp@live.com>
2023-11-17 23:50:40 +0800

    Add IEEE754 floating-point number viewer.

--- src/app/toolbox/module.ts
+++ src/app/toolbox/module.ts
@@ -1,5 +1,6 @@
 import meta from "@/app/toolbox/cidr-calculator/metadata";
+import fp_meta from "@/app/toolbox/ieee754/metadata";
 
-const exported_tools = [meta];
+const exported_tools = [meta, fp_meta];
 
 export default exported_tools;
