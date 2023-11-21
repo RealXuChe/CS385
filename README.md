9de94305785c049ae1524bf87f2adb4e0f1c84e9
Lightlyfruitshop <114576018+Lightlyfruitshop@users.noreply.github.com>
2023-11-21 22:04:49 +0800

    test by chen yuliang

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ We're the Team 14, *PHENOMENON*, consisting of the following members:
 >
 > YANG YUNFEI
 >
-> CHEN YULIANG in test
+> CHEN YULIANG
 >
 > YE JINGRUI
 >
---
+++ src/app/toolbox/QR-Code/metadata.tsx
@@ -0,0 +1,8 @@
+import {ToolMeta} from "@/inlcude/tool_metadata";
+
+const meta: ToolMeta = {
+    name: "QR Code",
+    route: "/toolbox/QR-Code",
+}
+
+export default meta;
