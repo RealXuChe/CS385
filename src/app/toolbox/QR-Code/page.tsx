254fd5fb4679e8185e332bc3ee6e875d0d118cd9
Lightlyfruitshop <114576018+Lightlyfruitshop@users.noreply.github.com>
2023-12-12 01:19:55 +0800

    feat: add a logo before convert

--- src/app/toolbox/QR-Code/page.tsx
+++ src/app/toolbox/QR-Code/page.tsx
@@ -2,6 +2,7 @@
 import { Button, TextField } from "@mui/material";
 import { useRef, useState } from "react";
 import QRCode from "qrcode.react";
+import Image from "next/image";
 
 export default function Home() {
   const [text, setText] = useState("");
@@ -32,23 +33,33 @@ export default function Home() {
             <QRCode value={text} />
           </div>
         )}
+        {!showQRCode && (
+          <div className="flex justify-center p-8" style={{ padding: 0 }}>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <Image
+              alt="QRCodeLogo"
+              src="/icon-QRcode.svg"
+              width={128}
+              height={128}
+            />
+          </div>
+        )}
         <div className="flex justify-center p-8">
-          {showQRCode && (
-            <Button
-              onClick={() => {
-                if (qrCodeRef.current) {
-                  // @ts-ignore
-                  const canvas = qrCodeRef.current.querySelector("canvas");
-                  const link = document.createElement("a");
-                  link.href = canvas.toDataURL("image/png");
-                  link.download = "qrcode.png";
-                  link.click();
-                }
-              }}
-            >
-              Download
-            </Button>
-          )}
+          <Button
+            onClick={() => {
+              if (qrCodeRef.current) {
+                // @ts-ignore
+                const canvas = qrCodeRef.current.querySelector("canvas");
+                const link = document.createElement("a");
+                link.href = canvas.toDataURL("image/png");
+                link.download = "qrcode.png";
+                link.click();
+              }
+            }}
+            disabled={!showQRCode}
+          >
+            Download
+          </Button>
         </div>
       </div>
     </div>
