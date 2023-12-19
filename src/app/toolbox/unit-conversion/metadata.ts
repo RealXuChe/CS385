00b8c82e7b0e4521d2ef826aa6abf44abd754af0
YangYunfei <yyfcpp@live.com>
2023-12-20 04:13:56 +0800

    add missing properties in metadata

--- src/app/toolbox/unit-conversion/metadata.ts
+++ src/app/toolbox/unit-conversion/metadata.ts
@@ -3,6 +3,8 @@ import { ToolMeta } from "@/inlcude/tool_metadata";
 const uc_meta: ToolMeta = {
   name: "Unit Conversion",
   route: "/toolbox/unit-conversion",
+  icon: "icon-uc.svg",
+  introduction: "This is an introduction",
 };
 
 export default uc_meta;
