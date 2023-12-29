aa6dca27077d97876fd3f2b283dc8e2a50f478dd
YangYunfei <yyfcpp@live.com>
2023-12-29 19:45:39 +0800

    delete cn comments

--- src/app/toolbox/QR-Code/page.tsx
+++ src/app/toolbox/QR-Code/page.tsx
@@ -11,8 +11,6 @@ export default function Home() {
   const [showQRCode, setShowQRCode] = useState(false);
   const qrCodeRef = useRef(null);
 
-  // 用于把一个日期格式化成 YYYY-MM-DD HH:MM:SS 的形式
-  // 可复制粘贴直接使用，不建议修改
   const formatDate = (date: Date) => {
     return (
       date.getFullYear() +
@@ -28,12 +26,9 @@ export default function Home() {
       ("0" + date.getSeconds()).slice(-2)
     );
   };
-  // 用于存储历史记录。
-  // 每一条询问和时间，都已数组形式存储。
   const saveHistory = (text: string) => {
     let rawInfo = localStorage.getItem(toolName);
     if (rawInfo == null) {
-      // 如果当前工具的历史记录是空的，则创建仅包含当前询问记录的数组
       let newInfo = {
         query: [text],
         time: [formatDate(new Date())],
@@ -41,7 +36,6 @@ export default function Home() {
       let newInfoStr = JSON.stringify(newInfo);
       localStorage.setItem(toolName, newInfoStr);
     } else {
-      // 如果当前工具已有历史记录，则追加当前询问记录到数组末尾
       let parsedInfo = JSON.parse(rawInfo);
       let queries = parsedInfo["query"];
       let times = parsedInfo["time"];
@@ -58,16 +52,12 @@ export default function Home() {
     }
   };
 
-  // 小工具处理询问、显示答案的代码部分
-  // 显示答案的同时，保存当前询问到历史记录中去
   const qrCodeGenerate = () => {
-    setInputText(text); // 显示答案
-    saveHistory(text); // 保存历史记录
+    setInputText(text); 
+    saveHistory(text); 
   };
 
-  // 处理从历史记录面板跳转来的部分
-  // 先从 URL 当中解析询问内容，然后根据询问内容，调用生成答案的代码
-  useEffect(() => {
+    useEffect(() => {
     const queryParams = new URLSearchParams(window.location.search);
     const history = queryParams.get("history");
     if (history != null) {
